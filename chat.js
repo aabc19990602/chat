a425242aebd9d7fdb3d737f75e16f03b056a3337
aabc19990602 <31418304+aabc19990602@users.noreply.github.com>
2017-08-29 00:21:35 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,47 @@
+"use strict";
+
+$(document).ready(function () {
+    var chatInterval = 250; 
+    var $userName = $("#userName");
+    var $chatOutput = $("#chatOutput");
+    var $chatInput = $("#chatInput");
+    var $chatSend = $("#chatSend");
+
+    function sendMessage() {
+        var userNameString = $userName.val();
+        var chatInputString = $chatInput.val();
+		document.getElementById("chatInput").value = ""; 
+
+        $.get("./write.php", {
+            username: userNameString,
+            text: chatInputString
+        });
+
+        $userName.val("");
+        retrieveMessages();
+    }
+
+    function retrieveMessages() {
+        $.get("./read.php", function (data) {
+            $chatOutput.html(data); 
+			
+        });
+		scrollSmoothToBottom ("chatOutput");
+    }
+
+
+    $chatSend.click(function () {
+        sendMessage();
+    });
+
+    setInterval(function () {
+        retrieveMessages();
+    }, chatInterval);
+});
+
+function scrollSmoothToBottom (id) {
+   var div = document.getElementById(id);
+   $('#' + id).animate({
+      scrollTop: div.scrollHeight - div.clientHeight
+   }, 100); 
+}
